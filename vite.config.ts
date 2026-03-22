ec8250c0d4f8e28dbaa9d4a2e624f752701c809a
Md. Tanvirul Islam <tanvirul.islam.swe@gmail.com>
2026-03-22 23:08:58 +0600

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+<<<<<<< ours
       '@': decodeURIComponent(new URL('./src', import.meta.url).pathname),
+=======
+      '@': new URL('./src', import.meta.url).pathname,
+>>>>>>> theirs
     },
   },
 });
